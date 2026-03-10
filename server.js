080b0c89f172f1f9d8e46ddfc7cd48f737dd22bd
tommiehansen31-ship-it <tommiehansen31@gmail.com>
2026-03-10 18:44:33 +0800

    Force IPv4 for SMTP connection

--- server.js
+++ server.js
@@ -28,13 +28,14 @@ EMAIL TRANSPORTER
 ========================================================= */
 
 const transporter = nodemailer.createTransport({
-host: process.env.EMAIL_HOST,
-port: 465,
-secure: true,
-auth: {
-user: process.env.EMAIL_USER,
-pass: process.env.EMAIL_PASS
-}
+  host: process.env.EMAIL_HOST,
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  },
+  family: 4
 });
 
 function sendShipmentEmail(receiverEmail, trackingNumber){
