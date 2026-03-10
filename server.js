3f5dbd2c27a297279e9af4b183ac3de1c9b9a73f
tommiehansen31-ship-it <tommiehansen31@gmail.com>
2026-03-10 12:27:21 +0800

    Fix shipment insert query with sender/receiver fields

--- server.js
+++ server.js
@@ -211,10 +211,9 @@ destination,
 shipmentname,
 weight,
 box_count,
-status,
-created_at
+status
 )
-VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,NOW())
+VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15)
 RETURNING id`,
 [
 trackingNumber,
